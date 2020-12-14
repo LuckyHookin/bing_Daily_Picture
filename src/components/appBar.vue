87bb17966f22ea37017d50be84b14a9a5f0d0927
LuckyHookin <luckyhookin@qq.com>
2020-12-14 22:02:32 +0800

    + datePicker

--- src/components/appBar.vue
+++ src/components/appBar.vue
@@ -1,16 +1,28 @@
 <template>
   <v-app-bar app>
-    <v-container class="py-0 fill-height">
+    <v-container class="pa-0 fill-height">
       <v-app-bar-nav-icon
         ><v-icon>mdi-microsoft-bing</v-icon></v-app-bar-nav-icon
       >
-      <v-toolbar-title>必应每日一图</v-toolbar-title>
+      <v-toolbar-title class="pl-2">必应壁纸集</v-toolbar-title>
       <v-spacer></v-spacer>
 
-      <v-btn text><v-icon>mdi-calendar-search</v-icon></v-btn>
+      
+      <v-menu open-on-hover offset-y :close-on-content-click="false" close-delay="200">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn small color="primary" outlined v-bind="attrs" v-on="on"><v-icon left>mdi-calendar-search</v-icon>{{date.toString()}}</v-btn>
+      </template>
+    <v-date-picker
+      v-model="date"
+      type="month"
+      min="2020-12"
+    ></v-date-picker>
+    </v-menu>
       <showMode></showMode>
     </v-container>
+    
   </v-app-bar>
+  
 </template>
 
 <script>
@@ -18,6 +30,21 @@ import showMode from "./showMode";
 export default {
   components: {
     showMode,
+  },
+  data() {
+      return {
+          date: "",
+      }
+  },
+  created () {
+      let thisDate=new Date();
+      this.date=thisDate.getUTCFullYear()+"-"+(thisDate.getUTCMonth()+1);
+  },
+  watch: {
+      date(newValue) {
+          newValue;
+          
+      }
   },
 };
 </script>
