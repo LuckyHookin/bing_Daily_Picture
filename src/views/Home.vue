8a5e713c3d0c039c99f2b09404654584d1892f9f
LuckyHookin <luckyhookin@qq.com>
2020-12-15 22:39:46 +0800

    + api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" sm="6" md="4" v-for="(item,index) in 7" :key="index">
-        <v-card hover><v-img src="https://cn.bing.com/th?id=OHR.PineGrosbeak_ZH-CN9629000282_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp"></v-img></v-card>
+      <v-col cols="12" sm="6" md="4" v-for="(item,index) in pictList" :key="index">
+        <v-card hover><v-img :src="item.url"></v-img></v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -25,7 +25,9 @@ export default {
   },
   watch: {
     date(newValue) {
-      this.axios.post("api/dailyPict/getBingPictByMonth",{month:newValue})
+      this.axios.get("dailyPict/getBingPictByMounth",{params:{date:newValue}}).then((e)=>{
+        this.pictList=e.data.list
+      })
     }
   },
 };
