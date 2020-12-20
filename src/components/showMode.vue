967e3c75bb1091d525b29f1872a2bba70dfc9569
LuckyHookin <luckyhookin@qq.com>
2020-12-20 22:52:38 +0800

    + 主题深浅缺省时跟随系统

--- src/components/showMode.vue
+++ src/components/showMode.vue
@@ -1,8 +1,7 @@
 <template>
-<v-btn icon class="ml-4" @click="showMode=!showMode">
+  <v-btn icon class="ml-4" @click="showMode = !showMode">
     <v-icon>mdi-brightness-4</v-icon>
-</v-btn>
-  
+  </v-btn>
 </template>
 
 <script>
@@ -15,7 +14,20 @@ export default {
   watch: {
     showMode(newValue) {
       this.$vuetify.theme.dark = newValue;
+      window.localStorage.removeItem("showMode");
+      window.localStorage.setItem("showMode", newValue);
     },
+  },
+  created() {
+    if (
+      window.localStorage.getItem("showMode") === null &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      this.showMode = true;
+    } else {
+      this.showMode =
+        window.localStorage.getItem("showMode") === "true" ? true : false;
+    }
   },
 };
 </script>
