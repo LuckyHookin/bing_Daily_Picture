93133793b466dd7eaa00d1ce84651e3c91d2ce52
LuckyHookin <luckyhookin@qq.com>
2020-12-21 10:04:31 +0800

    fix bug: showMode

--- src/components/showMode.vue
+++ src/components/showMode.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-btn icon class="ml-4" @click="showMode = !showMode">
+  <v-btn icon class="ml-4" @click="switchShowMode">
     <v-icon>mdi-brightness-4</v-icon>
   </v-btn>
 </template>
@@ -7,20 +7,32 @@
 <script>
 export default {
   data() {
-    return {
-      showMode: false,
-    };
+    return {};
   },
-  watch: {
-    showMode(newValue) {
-      this.$vuetify.theme.dark = newValue;
-      if (window.localStorage.getItem("showMode") !== null) {
-        window.localStorage.removeItem("showMode");
-        window.localStorage.setItem("showMode", newValue);
-      }
+  methods: {
+    switchShowMode() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+      window.localStorage.removeItem("showMode");
+      window.localStorage.setItem("showMode", this.$vuetify.theme.dark);
     },
   },
   created() {
+    let showMode = window.localStorage.getItem("showMode");
+    switch (showMode) {
+      case "true":
+        this.$vuetify.theme.dark = true;
+        break;
+      case "false":
+        this.$vuetify.theme.dark = false;
+        break;
+      default:
+        this.$vuetify.theme.dark = false;
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+          this.$vuetify.theme.dark = true;
+        }
+        break;
+    }
+
     if (
       window.localStorage.getItem("showMode") === null &&
       window.matchMedia("(prefers-color-scheme: dark)").matches
